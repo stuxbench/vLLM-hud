c112638dce5654248797deb09de45e88a56c8b06
Louis Sloot <lsloot@andrew.cmu.edu>
2025-10-02 16:32:28 -0400

    Make cves folder a package

---
+++ src/controller/cves/__init__.py
@@ -0,0 +1,5 @@
+"""CVE tools package.
+
+Modules in this package are auto-imported by src.controller.server on startup
+so that their @mcp.tool-decorated functions are registered with the server.
+"""
